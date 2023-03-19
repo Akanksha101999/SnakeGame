2e41a6b667946a3bbe36f0cb9b39b5c213e0f8c1
Akanksha101999 <97754410+Akanksha101999@users.noreply.github.com>
2023-03-19 23:04:12 +0530

    Add files via upload

---
+++ snakeGame/SnakeGame/src/frame.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class frame extends JFrame {
+	frame(){
+		//adding the panel in the frame
+		this.add(new panel());
+		//naming the frame window
+		this.setTitle("Snakegame");
+		//to make sure uniform game window size
+		this.setResizable(false);
+		this.setVisible(true);
+		this.pack();
+		
+	}
+
+}
